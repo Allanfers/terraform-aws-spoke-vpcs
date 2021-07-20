d2a35acf64d59045a0a8987df6be59d882196796
EC2 Default User <ec2-user@ip-172-31-70-216.ec2.internal>
2021-07-20 23:40:30 +0000

    testes

--- variables.tf
+++ variables.tf
@@ -1,42 +1,50 @@
 variable "name" {
+  description = "teste"
   type        = string
   default     = ""
 }
 
 variable "cidr" {
+  description = "teste"
   type        = string
   default     = "0.0.0.0/0"
 }
 
 variable "azs" {
+  description = "teste"
   type    = list(string)
   default = []
   
 }
 
 variable "use_az_id" {
+  description = "teste"
   type    = list(string)
   default = ["use1-az1", "use1-az2", "use1-az3", "use1-az4"]
   
 }
 
 variable "azs_count" {
+  description = "teste"
   type    = number
   default = "4"
   
 }
 
 variable "public_subnets" {
+  description = "teste"
   type        = list(string)
   default     = []
 }
 
 variable "private_subnets" {
+  description = "teste"
   type        = list(string)
   default     = []
 }
 
 variable "endpoint_subnets" {
+  description = "teste"
   type        = list(string)
   default     = []
 }
